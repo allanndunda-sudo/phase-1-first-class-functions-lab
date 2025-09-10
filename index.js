7f3901220880bdbc18aacc3a11a487058139d77b
allanndunda <allanndunda>
2025-09-10 20:52:07 +0300

    added

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+
+const returnFirstTwoDrivers= function(drivers){
+    return drivers.slice(0, 2);
+}
+
+
+const returnLastTwoDrivers= function(drivers){
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return function(fare){
+        return fare * multiplier;
+    };
+}
+
+const fareDouble = createNewMultiplier(2);
+
+const result= fareDouble(12);
+
+function fareDoubler(fare){
+    return fare * 2;
+}
+
+function fareTripler(fare){
+    return fare * 3;
+}
+
+function selectDifferentDrivers(drivers){
+    return drivers.slice(0, 2);
+}
+
+function selectDifferentDrivers(drivers){
+    return drivers.slice(-2);
+}
+
+function selectDifferentDrivers(drivers, fn){
+    return fn(drivers);
+}
